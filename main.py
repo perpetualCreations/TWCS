101b01369ab04c4150e9f8f43f701fa1c621994a
tchen <tchen0584@gmail.com>
2021-06-03 13:34:55 -0400

    More setup, completed Flask server and templates.

--- main.py
+++ main.py
@@ -7,14 +7,106 @@ Made by perpetualCreations
 import flask
 import flask_socketio
 import configparser
+import threading
+import requests
+import json
+import sqlite3
 from os import urandom
+from os.path import isfile
 from ast import literal_eval
+from time import sleep, time
 
 config = configparser.ConfigParser()
 config.read("main.cfg")
 application = flask.Flask(__name__)
 application.secret_key = urandom(4096)
 socket_io = flask_socketio.SocketIO(application)
+
+check_daemons: dict = {}
+
+if not isfile("eta.db"):
+    eta_database = sqlite3.connect("eta.db")
+    eta_database.cursor().execute('''CREATE TABLE eta (
+    nid INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    referrer VARCHAR(2000) NOT NULL,
+    eta INT DEFAULT NULL
+);''')
+else:
+    eta_database = sqlite3.connect("eta.db")
+
+
+@socket_io.on("requestETA")
+def eta_request_handler(json_payload) -> None:
+    """
+    Handle client requests for ETA times, when given a referrer URL.
+
+    :param json_payload: JSON request data from client
+    """
+    json_payload = json.loads(str(json_payload).replace("'", '"'))
+    flask_socketio.emit("postETA",
+                        {eta_database.cursor().execute(
+                            "SELECT * FROM eta WHERE referrer=:ref",
+                            {"ref": flask.request.referrer}).fetch()[2]})
+
+
+@socket_io.on("revealFate")
+def fate_broadcaster(fate: bool, url: str):
+    """
+    Broadcast to clients over web sockets the referrer fate.
+
+    The fate informs the client whether the referrer is up, or if maximum
+    ping attempts have occured.
+    The client will decide whether to abort with an error message, or redirect
+    back to referrer, based on the referrer fate.
+
+    :param fate: True for referrer being up, False for max ping attempts being
+        passed, and referrer still down
+    :type fate: bool
+    :param url: URL to referrer
+    :type url: str
+    """
+    with application.app_context():
+        flask_socketio.emit("revealFate", {"url": url, "fate": fate},
+                            broadcast=True)
+
+
+def check(url: str) -> None:
+    """
+    Check URL to see if referrer is up. If so, redirect clients.
+
+    :param url: URL to referrer
+    :type url: str
+    """
+    pings = 0
+    if "http" != url[:4]:
+        url = "http://" + url
+    init_time = time()
+    while True:
+        try:
+            status = requests.get(url).status_code
+        except Exception:
+            status = None
+        if status in (list(range(200, 227)) + [302, 401]):
+            exit_time = time() - init_time
+            eta_data = eta_database.cursor().execute(
+                "SELECT * FROM eta WHERE referrer=:ref",
+                {"ref": flask.request.referrer}).fetch()
+            if not eta_data:
+                eta_database.cursor().execute(
+                    "INSERT INTO eta VALUES (referrer, eta) VALUES" +
+                    " (:ref, :eta)", {"ref": url, "eta": time() - exit_time})
+            else:
+                eta_database.cursor().execute(
+                    "UPDATE eta SET eta=:eta WHERE referrer=:ref",
+                    {"ref": url, "eta": round((eta_data[2] + exit_time)/2)})
+            fate_broadcaster(True, url)
+            break
+        pings += 1
+        if int(config["CORE"]["PING_MAX"]) != -1 and \
+                pings >= int(config["CORE"]["PING_MAX"]):
+            fate_broadcaster(False, url)
+            break
+        sleep(int(config["CORE"]["DELAY_PING"]))
 
 
 @application.route("/")
@@ -25,6 +117,13 @@ def index() -> any:
     :return: any
     :rtype: any
     """
+    if flask.request.referrer not in check_daemons:
+        check_daemons.update(
+            {flask.request.referrer:
+                threading.Thread(target=check, args=(flask.request.referrer,))}
+            )
+    return flask.render_template(config["UI"]["TEMPLATE_NAME"],
+                                 referrer=flask.request.referrer)
 
 
 if __name__ == "__main__":
--- static/README.md
+++ static/README.md
@@ -1,5 +1,2 @@
 # README: DIRECTORY NOTICE
-When using custom stylesheets, insert stylesheet files into this directory.
-Edit the `STYLESHEET_NAME` field under section `UI` in the `main.cfg` file to the name of the stylesheet file.
-
-Alternatively, reference a stylesheet through a URL in the same field, and skip using this directory altogether when implementing custom stylesheets.
+When using custom templates, insert if applicable, their stylesheet files into this directory.
